58581ccc4917a7d0b8981aa64318df3d7e5cca50
Paola1899 <Paola1899>
2021-01-20 12:12:58 -0600

    Flask

--- new.py
+++ new.py
@@ -1,5 +1,15 @@
-def square(x):
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return "hola"
+
+
+'''def square(x):
    return x*x
 
 for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
+    print("{} squared is {}".format(i, square(i)))'''
+
